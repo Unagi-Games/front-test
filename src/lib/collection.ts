9e8ecbf8a5a5b76b2a5eae7b3278737f59565847
abdelali fajr <fajr.abdelali@outlook.com>
2024-06-02 19:11:45 +0200

    Modify fetchCollection to fetch the data from the server

--- src/lib/collection.ts
+++ src/lib/collection.ts
@@ -1,16 +1,10 @@
-export const fetchCollection = () => {
+//to fetch the data from the server
+export const fetchCollection = async () => {
   /**
    * Step 2: Instead of directly returning the collection, fetch it from http://localhost:8001/cards
    */
-  return [
-    {
-      id: 26166,
-      player: {
-        firstname: 'Karim',
-        lastname: 'Benzema',
-        birthday: '1987-12-19T08:38:50.090Z',
-        image: 'https://images.fotmob.com/image_resources/playerimages/26166.png'
-      }
-    }
-  ];
+
+  const response = await fetch('http://localhost:8001/cards');
+  const collection = await response.json();
+  return collection;
 };
