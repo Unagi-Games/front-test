7cc098347c3c5537d91685813c8c37b48e9b4709
abdelali fajr <fajr.abdelali@outlook.com>
2024-06-02 19:14:47 +0200

    Update Collection component to display the card data

--- src/pages/Collection.tsx
+++ src/pages/Collection.tsx
@@ -1,15 +1,45 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { fetchCollection } from '../lib/collection';
 
 import './Collection.css';
+import Card from '../types/interfaces/Card';
+
+
+
 
 export const Collection = () => {
-  const collection = fetchCollection();
-  const card = collection[0];
 
   /**
    * Step 1: Render the card
    */
-  return <div />;
+
+  const [collection, setCollection] = useState<Card[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const getCollection = async () => {
+      const data = await fetchCollection();
+      setCollection(data);
+      setLoading(false);
+    };
+    getCollection();
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+
+  const card = collection[0];
+
+  return (
+    <div className="card">
+      <img src={card.player.image} alt={`${card.player.firstname} ${card.player.lastname}`} />
+      <h2>{card.player.firstname} {card.player.lastname}</h2>
+      <p>DOB: {new Date(card.player.birthday).toLocaleDateString()}</p>
+    </div>
+  );
+
+
 };
