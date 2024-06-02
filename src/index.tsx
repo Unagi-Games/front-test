9b9d3b54e404e96832c7cd6e6ea8fd581c58a53d
andrei1892 <andreipiti17@gmail.com>
2024-06-02 16:00:08 +0300

    Step1 - render card

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,31 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 
 import { Collection } from './pages/Collection';
 import { CreateCard } from './pages/CreateCard';
 
+import './index.css';
+
+const Home = () => {
+  return (
+    <div className="home">
+      <ul>
+        <li>
+          <Link to="/collection">Collection</Link>
+        </li>
+        <li>
+          <Link to="/create-card">Create Card</Link>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
 const App = () => (
   <Router>
     <Switch>
+      <Route exact path="/" component={Home} />
       <Route exact path="/collection" component={Collection} />
       <Route exact path="/create-card" component={CreateCard} />
     </Switch>
--- src/pages/Collection.tsx
+++ src/pages/Collection.tsx
@@ -1,15 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 import { fetchCollection } from '../lib/collection';
 
+import { Loader } from '../components/Loader/Loader';
+
 import './Collection.css';
+
+// interface Player {
+//   id: number;
+//   firstname: string;
+//   lastname: string;
+//   birthday: string;
+//   image: string;
+// }
+
+// interface Card {
+//   id: number;
+//   player: Player;
+// }
 
 export const Collection = () => {
   const collection = fetchCollection();
   const card = collection[0];
+  const [isImageLoaded, setImageLoaded] = useState(false);
+  const { player } = card;
+  const birthdate: Date = new Date(player.birthday);
 
-  /**
-   * Step 1: Render the card
-   */
-  return <div />;
+  return (
+    <div className="card">
+      <div className="go-back">
+        <Link to="/">{`\<`} Back </Link>
+      </div>
+      <div className="card-container">
+        <p>{player.firstname}</p>
+        <p>{player.lastname}</p>
+        <div>
+          <p>
+            Birthday:
+            {birthdate.toLocaleDateString('default', {
+              day: 'numeric',
+              month: 'long',
+              year: 'numeric',
+            })}
+          </p>
+        </div>
+        <div>
+          <Loader loading={isImageLoaded} size={'small'}>
+            <img className="player-img" src={player.image} loading="lazy" />
+          </Loader>
+        </div>
+      </div>
+    </div>
+  );
 };
