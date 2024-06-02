125acde5971f108a4946b6489e633450f3f48cc3
abdelali fajr <fajr.abdelali@outlook.com>
2024-06-02 19:37:45 +0200

    Implement image fetching with the player's ID

--- src/pages/Collection.tsx
+++ src/pages/Collection.tsx
@@ -1,18 +1,12 @@
 import React, { useEffect, useState } from 'react';
 
 import { fetchCollection } from '../lib/collection';
-
 import './Collection.css';
 import Card from '../types/interfaces/Card';
 
 
 
-
 export const Collection = () => {
-
-  /**
-   * Step 1: Render the card
-   */
 
   const [collection, setCollection] = useState<Card[]>([]);
   const [loading, setLoading] = useState(true);
@@ -30,12 +24,14 @@ export const Collection = () => {
     return <div>Loading...</div>;
   }
 
+  const generateImageUrl = (id: number) => `https://images.fotmob.com/image_resources/playerimages/${id}.png`;
 
   const card = collection[0];
+  const imageUrl = generateImageUrl(card.id);
 
   return (
     <div className="card">
-      <img src={card.player.image} alt={`${card.player.firstname} ${card.player.lastname}`} />
+      <img src={imageUrl} alt={`${card.player.firstname} ${card.player.lastname}`} />
       <h2>{card.player.firstname} {card.player.lastname}</h2>
       <p>DOB: {new Date(card.player.birthday).toLocaleDateString()}</p>
     </div>
