734b4be8407186e04bc12578dc9ae00c282851e5
abdelali fajr <fajr.abdelali@outlook.com>
2024-06-03 03:27:29 +0200

    added aditional style to the card and date format

--- src/components/CardComponent.tsx
+++ src/components/CardComponent.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Card from '../types/interfaces/Card';
 import { LazyImage } from './LazyImage';
 import styled from 'styled-components';
+import { format } from 'date-fns';
 
 interface CardComponentProps {
     card: Card;
@@ -14,20 +15,36 @@ export const CardComponent = ({ card }: CardComponentProps) => {
 
     const imageUrl = generateImageUrl(card.id);
 
+    // Format the birthday using date-fns
+    const formattedDOB = format(new Date(card.player.birthday), 'MMMM do, yyyy');
+
     return (
         <CardWrapper>
             <LazyImage src={imageUrl} alt={`${card.player.firstname} ${card.player.lastname}`} />
-            <h2>{card.player.firstname} {card.player.lastname}</h2>
-            <p>DOB: {card.player.birthday}</p>
+            <H2>{card.player.firstname} {card.player.lastname}</H2>
+            <DOB>DOB: {formattedDOB}</DOB>
         </CardWrapper>
     );
 }
 
 const CardWrapper = styled.div`
-  border: 1px solid #ccc;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  border: 1px solid #4d4d4d;
   padding: 16px;
   margin: 16px;
+  background-color:#1d1e22;
   border-radius: 8px;
-  box-shadow: 0 2px 4px rgba(0,0,0,0.1);
-  text-align: center;
+  box-shadow: 0 5px 30px rgba(1, 1, 1, 0.3);
+`;
+
+const H2 = styled.h2`
+margin-top:.3rem;
+`;
+
+const DOB = styled.p`
+margin-top:.3rem;
+font-size:.8rem;
 `;
